6a87eb9451716a373ad8ef388c9dc122b6ca9b8e
gabgabgab <gabgabgab@DESKTOP-FLAV6BL>
2023-03-24 17:19:54 +0800

    Add project files.

---
+++ Gabriel_Llorico_ProSource/Program.cs
@@ -0,0 +1,72 @@
+﻿using Services;
+
+namespace Gabriel_Llorico_ProSource
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var isNullAllowed = true;
+            //List<string?> TestData() is populated below, you can add test cases below
+            foreach (var testString in TestData())
+            {
+                //to remove immediately null or empty or whitespace
+                if (string.IsNullOrWhiteSpace(testString))
+                {
+                    if (testString == null)
+                    {
+                        if (isNullAllowed)
+                        {
+                            Console.WriteLine("null will return true");
+                        }
+                        else
+                        {
+                            Console.WriteLine("null will return false");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{testString} will return true");
+                    }
+                    continue;
+                }
+
+                var removedUnnecessaryCharacters = StringManipulationHelpers.RemoveCharacters(testString);
+
+                if (string.IsNullOrWhiteSpace(removedUnnecessaryCharacters))
+                {
+                    Console.WriteLine($"{testString} will return true");
+                }
+                else
+                {
+                    if (StringManipulationHelpers.CheckIfBracketPlacementIsCorrect(removedUnnecessaryCharacters))
+                    {
+                        Console.WriteLine($"{testString} will return true");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{testString} will return false");
+                    }
+                }
+            }
+        }
+
+        private static List<string?> TestData()
+        {
+            return new List<string?>
+            {
+                "{}", //will return true (closed set of bracket)
+                "}{", //will return false (closed bracket cant proceed all open brackets)
+                "{{}", //will return false (one bracket pair was not closed)
+                "", //will return true (empty string will return true)
+                "{abcdE1234.}", //will return true (non bracket will be ignored)
+                "{{a}{b{c}d}E1234.}", //will return true (perfectly closed brackets)
+                "{{}}}{", //will return false (equal brackets but one is open)
+                "abcdE1234.", //will return true (ignore characters)
+                "{a}bc}De{12{34.}", //will return false (unclosed bracket)
+                "{}}{{}", //will return false (equal sets but not closed)
+                null //will return true or false dependent on the value listed on the variable isNullAllowed
+            };
+        }
+    }
+}
---
+++ Helpers/StringManipulationHelpers.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Services
+{
+    public class StringManipulationHelpers
+    {
+        public static string RemoveCharacters(string input)
+        {
+            return Regex.Replace(input, @"[^{}]", string.Empty);
+        }
+
+        public static bool CheckIfBracketPlacementIsCorrect(string input)
+        {
+            var stringLength = input.Length;
+            var validCombination = false;
+
+            //Checks if first character is } or last character is { and it will return false
+            //Checks if length of trimmed characters is odd then it will return false
+            //Check if number of { is not equal to number of } then it will return false
+            if (input[0].ToString() == "}" || input[stringLength - 1].ToString() == "{" || stringLength % 2 != 0 ||
+                input.Count(x => x.ToString() == "}") != input.Count(x => x.ToString() == "{"))
+            {
+                return validCombination;
+            }
+
+            var notStopChecking = true;
+
+            //keeps deleting till input.length is zero or previous before cleanup is equal to cleaned up string
+            while (notStopChecking)
+            {
+                var beforeRemoval = input;
+                input = input.Replace("{}", "");
+                
+                //checks if before removing {} is equal to after removing {}, meaning no more set of closed braces
+                if (beforeRemoval == input)
+                {
+                    validCombination = false;
+                    notStopChecking = false;
+                }
+                //checks if all perfectly closed brackets are removed, meaning it is a perfect set of brackets
+                if (input.Length == 0)
+                {
+                    validCombination = true;
+                    notStopChecking = false;
+                }
+            }
+
+            return validCombination;
+        }
+    }
+}
